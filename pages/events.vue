5f21a2a5f0dcca7a989fbc16ae54ed8dd48153be
Zsuzsa.L <manoaway@gmail.com>
2026-09-16 22:14:29 +0200

    feat(pages) events

---
+++ pages/events.vue
@@ -0,0 +1,24 @@
+<template>
+  <main id="main" class="typography">
+    <span>{{ events.title }}</span>
+    <MDC :value="events.content" />
+  </main>
+</template>
+
+<script setup>
+const { data: events } = reactive(
+  await useAsyncData("events", () => queryContent("/pages/events").findOne()),
+);
+</script>
+
+<style lang="scss" scoped>
+main {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+
+  :deep(div) {
+    max-width: 500px;
+  }
+}
+</style>
