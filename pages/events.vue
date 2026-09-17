777bc7ef7033e399a01821475d0617a17f84f5c3
Zsuzsa.L <manoaway@gmail.com>
2026-09-17 12:36:45 +0200

    feat(events) show text

--- pages/events.vue
+++ pages/events.vue
@@ -1,9 +1,11 @@
 <template>
   <main id="main" class="events">
     <span>{{ events.title }}</span>
-    <MDC :value="events.subTitle1" />
+
+    <span>{{ events.subTitle1 }}</span>
     <MDC :value="events.content1" />
-    <MDC :value="events.subTitle2" />
+
+    <span>{{ events.subTitle2 }}</span>
     <MDC :value="events.content2" />
   </main>
 </template>
