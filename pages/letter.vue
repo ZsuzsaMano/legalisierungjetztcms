7f510bf935f00fe5b8b8b662f6703603efee8b3c
Zsuzsa.L <manoaway@gmail.com>
2026-09-24 16:37:56 +0200

    fix(cms): fix letter content fetching

--- pages/letter.vue
+++ pages/letter.vue
@@ -1,14 +1,20 @@
 <template>
   <main id="main" class="letter">
-    <h3>{{ letter.title }}</h3>
-    <MDC :value="letter.content" />
+    <h3>{{ currentContent.title }}</h3>
+    <MDC :value="currentContent.content" />
   </main>
 </template>
 
 <script setup>
 const { data: letter } = reactive(
-  await useAsyncData("letter", () => queryContent("/pages/letter").findOne()),
+  await useAsyncData("letter", () => queryContent("letter").findOne()),
 );
+
+const { locale } = useI18n();
+
+const currentContent = computed(() => {
+  return letter?.[locale.value] || letter?.de || {};
+});
 </script>
 
 <style lang="scss" scoped>
